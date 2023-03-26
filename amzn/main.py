412eaf23ac1760857c4eacbcc618412b17fb29f5
jit-shlomi <shlomi@jit.io>
2023-03-26 18:34:14 +0300

    typo

--- amzn/main.py
+++ amzn/main.py
@@ -3,9 +3,9 @@ import os
 
 if __name__ == "__main__":
     print('Checking mount status')
-    if os.path.ismount(f"s3_bucket/"):
-        print(f"bucket mounted successfully :)")
+    if os.path.ismount("s3_bucket/"):
+        print("bucket mounted successfully :)")
     else:
-        print(f"bucket not mounted :(")
+        print("bucket not mounted :(")
     print("list s3 directories")
-    print(os.listdir(f"s3_bucket/"))
+    print(os.listdir("s3_bucket/"))
